7b0667d4fae5f5cbffd96660bcc55a195b99f9f5
Rentoo <dreamYiZ@outlook.com>
2025-02-09 21:45:41 +0800

    -

--- lib/message_detail_page.dart
+++ lib/message_detail_page.dart
@@ -1,21 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-class MessageDetailPage extends StatelessWidget {
+class MessageDetailPage extends StatefulWidget {
   final String title;
   final String content;
 
   const MessageDetailPage({Key? key, required this.title, required this.content}) : super(key: key);
 
   @override
+  _MessageDetailPageState createState() => _MessageDetailPageState();
+}
+
+class _MessageDetailPageState extends State<MessageDetailPage> {
+  final controller = WebViewController()
+  ..setJavaScriptMode(JavaScriptMode.unrestricted)
+  ..setNavigationDelegate(
+    NavigationDelegate(
+      onProgress: (int progress) {
+        // Update loading bar.
+      },
+      onPageStarted: (String url) {},
+      onPageFinished: (String url) {},
+      onHttpError: (HttpResponseError error) {},
+      onWebResourceError: (WebResourceError error) {},
+      onNavigationRequest: (NavigationRequest request) {
+        if (request.url.startsWith('https://www.youtube.com/')) {
+          return NavigationDecision.prevent;
+        }
+        return NavigationDecision.navigate;
+      },
+    ),
+  )
+  ..loadRequest(Uri.parse('https://flutter.dev'));
+
+
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Text(content),
-      ),
+      body: WebViewWidget(controller: controller),
     );
   }
 }
